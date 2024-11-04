e1367b8b6669c6c1ac476cf31aa48ecea4cbc735
risan <potallegta@gmail.com>
2024-11-04 15:54:49 +0700

    fix: pinia init

--- plugins/pinia.ts
+++ plugins/pinia.ts
@@ -1,8 +1,13 @@
 // plugins/pinia.ts
 import { defineNuxtPlugin } from "#app";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const pinia = createPinia();
+
+  if (import.meta.client) {
+    pinia.use(piniaPluginPersistedstate);
+  }
   nuxtApp.vueApp.use(pinia);
 });
