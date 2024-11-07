e48bf4aa8957081350bdb8aff2ed31078cdf1b3c
risan <potallegta@gmail.com>
2024-11-07 14:55:26 +0700

    feat: loading on initial page load

--- app.vue
+++ app.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="toast-container" class="fixed right-5 top-5 space-y-2" />
   <NuxtPage v-if="isMounted" />
+
+  <div v-else class="flex h-screen w-screen items-center justify-center">
+    <LoadingCircle />
+  </div>
 </template>
 
 <script setup lang="ts">
+import LoadingCircle from "./components/LoadingCircle.vue";
 import { useAuthStore } from "./stores/auth";
 const { isAuthenticated, fetchUser } = useAuthStore();
 const isMounted = ref(false);
