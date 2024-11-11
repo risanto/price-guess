8a95d65fef62c6e7823b08e68fe55631a381f908
risan <potallegta@gmail.com>
2024-11-11 13:51:52 +0700

    feat: api/config/current-content headers

--- server/api/config/current-content/index.get.ts
+++ server/api/config/current-content/index.get.ts
@@ -13,12 +13,15 @@ function getTodayMidnight() {
 export default eventHandler(
   async (event): Promise<ApiResponse<{ id: number }>> => {
     // Set headers to prevent caching
-    event.node.res.setHeader(
-      "Cache-Control",
-      "no-store, no-cache, must-revalidate, proxy-revalidate",
-    );
+    event.node.res.setHeader("Surrogate-Control", "no-store");
+    event.node.res.setHeader("Cache-Control", "private, no-store, max-age=0");
     event.node.res.setHeader("Pragma", "no-cache");
     event.node.res.setHeader("Expires", "0");
+
+    console.log(
+      "/api/config/current-content headers:",
+      event.node.res.getHeaders(),
+    );
 
     const client = serverSupabaseServiceRole<Database>(event);
     const config = useRuntimeConfig();
