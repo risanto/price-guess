26778d9a4b2b4d77b804563a40ef9608f4134000
risan <potallegta@gmail.com>
2024-12-10 13:59:04 +0700

    feat: separate rate limit for authorized & unauthorized users

--- server/middleware/rateLimit.ts
+++ server/middleware/rateLimit.ts
@@ -2,10 +2,14 @@
 import { RateLimiterMemory } from "rate-limiter-flexible";
 import { defineEventHandler } from "h3";
 
-// Configure rate limiting
-const rateLimiter = new RateLimiterMemory({
-  points: 30, // 30 requests
-  duration: 60, // per 60 seconds
+const rateLimiterAuthenticated = new RateLimiterMemory({
+  points: 10, // 10 requests per 60 seconds
+  duration: 60,
+});
+
+const rateLimiterUnauthenticated = new RateLimiterMemory({
+  points: 30, // Allow more requests for unauthenticated users
+  duration: 60,
 });
 
 // Middleware function
@@ -14,11 +18,18 @@ export default defineEventHandler(async (event) => {
   const ip =
     (typeof ipHeader === "string" ? ipHeader.split(",")[0] : ipHeader?.[0]) ||
     event.node.req.socket.remoteAddress;
+  const authHeader = event.node.req.headers["authorization"];
 
-  const userKey = event.node.req.headers["authorization"] || ip;
+  const userKey = authHeader ? authHeader : ip;
 
   try {
-    await rateLimiter.consume(userKey as string);
+    if (authHeader) {
+      // If user is authenticated, apply a stricter rate limit
+      await rateLimiterAuthenticated.consume(userKey as string);
+    } else {
+      // If user is not authenticated, allow more requests for the same IP
+      await rateLimiterUnauthenticated.consume(userKey as string);
+    }
   } catch (err) {
     const error = createError({
       statusCode: 429,
